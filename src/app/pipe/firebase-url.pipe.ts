62b8a6dc4061542799217b23b76fed42e30fca7e
renico <n.renard@gmail.com>
2017-04-11 17:58:04 +0200

    comment unused getBase64Image function

--- src/app/pipe/firebase-url.pipe.ts
+++ src/app/pipe/firebase-url.pipe.ts
@@ -23,16 +23,16 @@ export class FirebaseUrlPipe implements PipeTransform {
     }
   }
 
-  gatBase64Image(img) {
-    const canvas = document.createElement('canvas');
-    canvas.width = img.width;
-    canvas.height = img.height;
+  // getBase64Image(img) {
+  //   const canvas = document.createElement('canvas');
+  //   canvas.width = img.width;
+  //   canvas.height = img.height;
 
-    const ctx = canvas.getContext('2d');
-    ctx.drawImage(img, 0, 0);
+  //   const ctx = canvas.getContext('2d');
+  //   ctx.drawImage(img, 0, 0);
 
-    const dataURL = canvas.toDataURL('image/png');
-    return dataURL.replace(/^data:image\/(png|jpg);base64,/, '');
-  }
+  //   const dataURL = canvas.toDataURL('image/png');
+  //   return dataURL.replace(/^data:image\/(png|jpg);base64,/, '');
+  // }
 
 }
